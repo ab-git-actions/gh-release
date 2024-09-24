64ddf23673a6a620e199262ff2741f779ce55847
aakashbasa <aakashbasa2538@gmail.com>
2024-09-24 13:48:44 -0400

    refactor code

--- src/create-release.js
+++ src/create-release.js
@@ -1,7 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const semver = require('semver');
-// const { GitHub, context } = require('@actions/github');
 
 
 const gitToken = core.getInput('github_token', {required: false});
@@ -10,19 +9,26 @@ const octokit = github.getOctokit(gitToken);
 // Fetch the latest Git tag from the repository
 async function getLatestTag(owner, repo) {
     try {
-        const tags = await octokit.rest.repos.listTags({
+        const {data: tags} = await octokit.rest.repos.listTags({
             owner,
             repo,
             per_page: 1 // Get only the latest tag
         });
 
-        if (tags.data.length === 0) {
+        if (tags.length === 0) {
             core.info('\u001b[38;5;6mNo tags found in the repository.');
             return null;
         }
 
-        const latestTag = tags.data[0].name;
+        const latestTag = tags[0].name;
+
+        // Loop through the tags and log each one
+        tags.forEach((tag) => {
+            core.info(`\u001b[35mExisting GitHub tags: ${tag.name}`);
+        });
+
         core.info(`\u001b[38;5;6mLatest tag: ${latestTag}`);
+
         return latestTag;
     } catch (error) {
         core.error(`\u001b[38;2;255;0;0mError fetching tags: ${error.message}`);
@@ -33,38 +39,71 @@ async function getLatestTag(owner, repo) {
 // Fetch the commit messages since the latest tag
 async function getCommitsSinceTag(owner, repo, latestTag) {
     try {
-        // Fetch the commits after the latest tag up to HEAD
-        const commits = await octokit.rest.repos.listCommits({
+
+        const { data: { sha: tagCommitSha } } = await octokit.rest.repos.getCommit({
             owner,
             repo,
-            // Provide the reference range: latestTag..HEAD
-            sha: 'HEAD',
-            per_page: 100, // Adjust as necessary, default is 30
+            ref: latestTag,
         });
+
+        // Fetch the commits after the latest tag up to HEAD
+        const {data: commits} = await octokit.rest.repos.listCommits({
+            owner,
+            repo,
+            sha: 'HEAD',
+            per_page: 100, // Adjust as necessary
+        });
+
 
         // Filter commits after the latest tag
         const commitMessages = [];
+        const prs = [];
         let foundLatestTag = false;
 
-        for (const commit of commits.data) {
+        const associatedPRs = octokit.rest.repos.listPullRequestsAssociatedWithCommit({
+            owner,
+            repo,
+            commit_sha: commit.sha,
+        });
+
+
+
+        // Await all promises at once
+        const prResults = await Promise.all(prPromises);
+
+        prResults.forEach((associatedPRs, index) => {
+            const commit = commits[index];
             const commitMessage = commit.commit.message;
 
+
             // Stop adding commits once the latest tag is found in commit history
-            if (commit.sha === latestTag || commitMessage.includes(latestTag)) {
+            if (commit.sha === tagCommitSha) {
                 foundLatestTag = true;
-                break;
+                return;
+            }
+
+            if (associatedPRs.data.length > 0) {
+                associatedPRs.data.forEach((pr) => {
+                    prs.push({
+                        number: pr.number,
+                        title: pr.title,
+                        user: pr.user.login,
+                    });
+                });
             }
 
             // Add commits that are after the latest tag
             commitMessages.push(commitMessage);
-        }
+
+        });
+
 
         if (!foundLatestTag) {
-            console.log("Warning: Could not find the latest tag in the commit history.");
+            core.warning("\u001b[38;2;255;0;0mWarning: Could not find the latest tag in the commit history.");
         }
 
-        console.log(`Commits since ${latestTag}:`, commitMessages);
-        return commitMessages;
+        core.info(`\u001b[38;5;6mCommits since ${latestTag}:, ${commitMessages}`);
+        return [commitMessages, prs];
     } catch (error) {
         core.error(`\u001b[38;2;255;0;0mError fetching commits: ${error.message}`);
         core.setFailed(`Error fetching commits: ${error.message}`);
@@ -94,7 +133,10 @@ function determineBumpType(commits) {
 
 // Calculate the next version using semver
 function calculateNextVersion(latestTag, bumpType) {
-    /**@type string*/
+    /**
+     * @type {string}
+     * @returns A version to be used for gitHub create release tag
+     */
     const newVersion = semver.inc(latestTag, bumpType);
     core.info(`\u001b[38;5;6mNew version: ${newVersion}`);
     return newVersion;
@@ -107,25 +149,26 @@ async function run() {
         const latestTag = await getLatestTag(gitOrg, gitRepo);
 
         if (latestTag) {
-            // Step 2: Get commits since the latest tag
-            const commits = await getCommitsSinceTag(gitOrg, gitRepo, latestTag);
+            // Get commits since the latest tag
+            const [commits, prs] = await getCommitsSinceTag(gitOrg, gitRepo, latestTag);
 
-            // Step 3: Determine the bump type based on commit messages
+            // Determine the bump type based on commit messages
             const bumpType = determineBumpType(commits);
 
-            // Step 4: Calculate the next version
+            // Calculate the next version
             const newVersion = calculateNextVersion(latestTag, bumpType);
 
-            // Get existing git release tags from where the action is being executed
-            const {data: tags} =  await octokit.rest.repos.listTags({
-                owner: gitOrg,
-                repo: gitRepo
-            });
+            // Create release body with PR information
+            let releaseBody = `### Changes since ${latestTag}\n\n`;
+            if (prs.length > 0) {
+                prs.forEach((pr) => {
+                    releaseBody += `- PR #${pr.number}: ${pr.title} by @${pr.user}\n`;
+                });
+            } else {
+                releaseBody += "No new PRs since the latest tag.";
+            }
 
-            // Loop through the tags and log each one
-            tags.forEach((tag) => {
-                core.info(`\u001b[35mExisting GitHub tags: ${tag.name}`);
-            });
+            core.info(`\x1b[38;5;214mRelease body content:\n${releaseBody}`);
 
             const pushEvent = (github.context.eventName === 'push')
 
@@ -134,36 +177,25 @@ async function run() {
 
             if (pushEvent && (branchName === 'main' || branchName === 'master')) {
 
-                const createRelease= await octokit.rest.repos.createRelease({
+                const createRelease = await octokit.rest.repos.createRelease({
                     owner: gitOrg,
                     repo: gitRepo,
                     tag_name: newVersion,
                     target_commitish: branchName,
                     name: newVersion,
-                    body: "Testing",
+                    body: releaseBody,
                     prerelease: false,
                     make_latest: 'true'
                 })
 
-                const {
-                    data:
-                        {
-                            id: releaseId,
-                            html_url: releaseUrl,
-                        }
-                } = createRelease;
-                core.info(releaseId);
-                core.info(releaseUrl);
+                const { data: { html_url: releaseUrl } } = createRelease;
+
+                core.info(`\u001b[35m${releaseUrl}`);
 
                 // Set the output variables for use by other actions: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
                 core.setOutput("GitHub Release URL", releaseUrl)
             }
         }
-
-        // const latestSha = github.context.sha;
-        // const prNumber = github.context.payload.pull_request.number
-        // const prTitle = github.context.payload.pull_request.tittle;
-        // const releaseBody = prTitle + "-" + prNumber;
 
     }catch(error) {
         core.setFailed(`Action failed with error: ${error.message}`);
